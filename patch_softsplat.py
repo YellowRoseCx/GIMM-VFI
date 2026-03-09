146f469909e11a7a7063586b72235767b539c4a4
YellowRoseCx <80486540+YellowRoseCx@users.noreply.github.com>
2026-03-09 13:14:04 -0500

    remove pycache

--- patch_softsplat.py
+++
@@ -1,6 +0,0 @@
-import re
-
-with open("src/models/generalizable_INR/modules/softsplat.py", "r") as f:
-    code = f.read()
-
-# Let's just rewrite the whole file with the clamp fixes applied directly, this avoids annoying regex.
